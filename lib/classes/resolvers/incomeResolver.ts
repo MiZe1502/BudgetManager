f3396e1e9cdeb2cde71c54f595c152e9913688e5
mzykov <zykov.mikhail@synterramedia.ru>
2019-07-10 18:39:22 +0300

    add mutations to incomeTypeResolver

--- lib/classes/resolvers/incomeResolver.ts
+++ lib/classes/resolvers/incomeResolver.ts
@@ -8,30 +8,27 @@ import { IRepository, RepositoryFactory, repositoryType } from "../repositories/
 export class IncomeResolver {
 
 	private repo: IncomeRepository = RepositoryFactory.createRepository(repositoryType.IncomeRepository) as IncomeRepository;
+
 	// Queries
 
 	@Query((returns) => [IncomeEntity])
 	public async incomes(): Promise<IncomeEntity[]> {
-		// const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeRepository);
 		return await this.repo.find({relations: ["type"]});
 	}
 
 	@Query((returns) => IncomeEntity)
 	public async incomeById(@Arg("id") id: number): Promise<IncomeEntity> {
-		// const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeRepository);
 		return await this.repo.findById(id);
 	}
 
 	@Query((returns) => IncomeEntity)
 	public async incomeByName(@Arg("name") id: string): Promise<IncomeEntity> {
-		// const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeRepository);
 		return await this.repo.findById(name);
 	}
 
 	@Query((returns) => [IncomeEntity])
 	public async incomesByType(@Arg("typeName", { nullable: true }) typeName?: string,
 							                     @Arg("typeId", { nullable: true }) typeId?: number): Promise<IncomeEntity[]> {
-		// const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeRepository);
 
 		return await this.repo.findByType(typeId, typeName);
 	}
@@ -39,14 +36,12 @@ export class IncomeResolver {
 	// Mutations
 
 	@Mutation((returns) => Int)
-	public async removeById(@Arg("id") id: number): Promise<number> {
-		// const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeRepository);
+	public async removeIncomeById(@Arg("id") id: number): Promise<number> {
 		return await this.repo.removeById(id);
 	}
 
 	@Mutation((returns) => Int)
-	public async removeByName(@Arg("name") name: string): Promise<number> {
-		// const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeRepository);
+	public async removeIncomeByName(@Arg("name") name: string): Promise<number> {
 		return await this.repo.removeByName(name);
 	}
 }
--- lib/classes/resolvers/incomeTypeResolver.ts
+++ lib/classes/resolvers/incomeTypeResolver.ts
@@ -1,4 +1,4 @@
-import { Arg, FieldResolver, Mutation, Query, Resolver, Root } from "type-graphql";
+import { Arg, FieldResolver, Int, Mutation, Query, Resolver } from "type-graphql";
 import { IncomeType } from "../entities/incomeEntity";
 import { IncomeTypeRepository } from "../repositories/incomeTypesRepository";
 import { IRepository, RepositoryFactory, repositoryType } from "../repositories/repositoryFactory";
@@ -6,24 +6,35 @@ import { IRepository, RepositoryFactory, repositoryType } from "../repositories/
 @Resolver()
 export class IncomeTypeResolver {
 
+	private repo: IncomeTypeRepository = RepositoryFactory.createRepository(repositoryType.IncomeTypeRepository) as IncomeTypeRepository;
+
+	// Queries
+
 	@Query((returns) => [IncomeType])
 	public async incomeTypes(): Promise<IncomeType[]> {
-		const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeTypeRepository);
-		return await (repo as IncomeTypeRepository).find();
+		return await this.repo.find();
 	}
 
 	@Query(((returns) => IncomeType))
 	public async incomeTypeById(@Arg("id") id: number): Promise<IncomeType> {
-		const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeTypeRepository);
-		return await (repo as IncomeTypeRepository).findById(id);
+		return await this.repo.findById(id);
 	}
 
 	@Query(((returns) => IncomeType))
 	public async incomeTypeByName(@Arg("name") name: string): Promise<IncomeType> {
-		const repo: IRepository = RepositoryFactory.createRepository(repositoryType.IncomeTypeRepository);
-		return await (repo as IncomeTypeRepository).findByName(name);
+		return await this.repo.findByName(name);
 	}
 
-	// TODO: Доделать мутации для удаления и добавления
+	// Mutations
+
+	@Mutation((returns) => Int)
+	public async removeIncomeTypeById(@Arg("id") id: number): Promise<number> {
+		return await this.repo.removeById(id);
+	}
+
+	@Mutation((returns) => Int)
+	public async removeIncomeTypeByName(@Arg("name") name: string): Promise<number> {
+		return await this.repo.removeByName(name);
+	}
 
 }
