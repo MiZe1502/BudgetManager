b416e271ca0e2d73caf9a3d4ad3d1aab9cfb1cb1
mzykov <zykov.mikhail@synterramedia.ru>
2019-08-05 14:21:55 +0300

    customized links

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -1,86 +1,56 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import './style.css'
+import sections from '../../helpers/sections'
 import { connect } from 'react-redux'
 import { updateSidebarState } from '../../redux/actions/sidebar'
 import { updateCurrentSection } from '../../redux/actions/sections'
 import { Link } from 'react-router-dom'
 
 
-const sections = [
-    {
-        text: 'Покупки',
-        url: '/'
-    },
-    {
-        text: 'Доходы',
-        url: '/income'
-    },
-    {
-        text: 'Категории',
-        url: '/category'
-    },
-    {
-        text: 'Магазины',
-        url: '/shops'
-    },
-    {
-        text: 'Статистика',
-        url: '/stats'
-    },
-]
-
-
 function SideBar(props) {
 
-  const toggleDrawer = (open) => event => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
+    const toggleDrawer = (open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+        }
+        props.updateSidebarState(open)
+    };
+
+    const handleLinkClick = (section) => event => {
+        props.updateCurrentSection(section)
     }
-    props.updateSidebarState(open)
-  };
-
-  const handleLinkClick = (section) => event => {
-    props.updateCurrentSection(section)
-  }
  
-  const sideList = () => (
-    <div
-        className='sidebar-menu'
-        role="presentation"
-        onClick={toggleDrawer(false)}
-        onKeyDown={toggleDrawer(false)}>
-      <List>
-        {sections.map((elem, index) => (
-          <ListItem button key={elem.text}>
-            <Link className="sidebar-link" to={elem.url} onClick = {handleLinkClick(elem.text)}>{elem.text}</Link>
-            {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
-            {/* <ListItemText primary={elem.text} /> */}
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-    </div>
-  );
+    const sideList = () => (
+        <div
+            className='sidebar-menu'
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}>
+            <List>
+                {sections.map((elem, index) => {
+                return elem.text === 'divider' ? <Divider /> : (
+                    <ListItem button key={elem.text}>
+                    <Link className="sidebar-link" to={elem.url} onClick = {handleLinkClick(elem.text)}>{elem.text}</Link>
+                    </ListItem>
+                )
+                })}
+            </List>
+        </div>
+    );
 
-  console.log(props.open)
+    console.log(props.open)
 
-  return (
-    <div>
-      <Drawer open={props.open} onClose={toggleDrawer(false)}>
-        {sideList()}
-      </Drawer>
-    </div>
-  );
+    return (
+        <div>
+            <Drawer open={props.open} onClose={toggleDrawer(false)}>
+                {sideList()}
+            </Drawer>
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
---
+++ client/src/helpers/sections.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        text: 'Покупки',
+        url: '/'
+    },
+    {
+        text: 'Доходы',
+        url: '/income'
+    },
+    {
+        text: 'Категории',
+        url: '/category'
+    },
+    {
+        text: 'Магазины',
+        url: '/shops'
+    },
+    {
+        text: 'divider'
+    },
+    {
+        text: 'Статистика',
+        url: '/stats'
+    },
+]
